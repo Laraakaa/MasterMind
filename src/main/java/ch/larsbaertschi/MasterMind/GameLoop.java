a4e9106baeef9f1c0522f6fcd9cd043dc0bf1342
Lars Bärtschi <lars.baertschi@bluewin.ch>
2017-08-17 11:16:04 +0200

    + Added gameLoop, made executable

---
+++ src/main/java/ch/larsbaertschi/MasterMind/GameLoop.java
@@ -0,0 +1,4 @@
+package ch.larsbaertschi.MasterMind;
+
+public class GameLoop {
+}
---
+++ src/main/java/ch/larsbaertschi/MasterMind/Main.java
@@ -0,0 +1,7 @@
+package ch.larsbaertschi.MasterMind;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting MasterMind by Lars Bärtschi");
+    }
+}
